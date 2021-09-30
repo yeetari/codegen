b973b2f20a0f8436f57eb50459fe51870ea146dc
Owen Smith <yeeetari@gmail.com>
2021-09-30 12:58:49 +0100

    support: Queue improvements

--- include/codegen/support/Queue.hh
+++ include/codegen/support/Queue.hh
@@ -7,13 +7,15 @@ class Queue {
     std::vector<T> m_storage;
 
 public:
-    void push(T elem) {
-        m_storage.insert(m_storage.begin(), elem);
+    void push(const T &elem) { m_storage.insert(m_storage.begin(), elem); }
+    void push(T &&elem) { m_storage.insert(m_storage.begin(), std::move(elem)); }
+    const T &peek() const { return m_storage.back(); }
+    T pop() {
+        auto ret = std::move(m_storage.back());
+        m_storage.pop_back();
+        return std::move(ret);
     }
 
-    T pop() {
-        auto ret = m_storage.back();
-        m_storage.pop_back();
-        return ret;
-    }
+    bool empty() const { return m_storage.empty(); }
+    auto size() const { return m_storage.size(); }
 };
