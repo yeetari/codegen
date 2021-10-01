1cdc48c7bd755983e9ee36d5cdd5553b0c886377
Owen Smith <yeeetari@gmail.com>
2021-10-01 12:26:15 +0100

    support: Rename Queue to Stack

--- include/codegen/support/Stack.hh
+++ include/codegen/support/Stack.hh
@@ -3,12 +3,12 @@
 #include <vector>
 
 template <typename T>
-class Queue {
+class Stack {
     std::vector<T> m_storage;
 
 public:
-    void push(const T &elem) { m_storage.insert(m_storage.begin(), elem); }
-    void push(T &&elem) { m_storage.insert(m_storage.begin(), std::move(elem)); }
+    void push(const T &elem) { m_storage.push_back(elem); }
+    void push(T &&elem) { m_storage.push_back(std::move(elem)); }
     const T &peek() const { return m_storage.back(); }
     T pop() {
         auto ret = std::move(m_storage.back());
