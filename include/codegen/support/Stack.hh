d1f9d15abf37dfe857323435204156e8a529597c
Owen Smith <yeeetari@gmail.com>
2021-10-01 20:45:06 +0100

    support: Add emplace and non-const peek functions to Stack

--- include/codegen/support/Stack.hh
+++ include/codegen/support/Stack.hh
@@ -7,8 +7,13 @@ class Stack {
     std::vector<T> m_storage;
 
 public:
+    template <typename... Args>
+    void emplace(Args &&...args) {
+        m_storage.emplace_back(std::forward<Args>(args)...);
+    }
     void push(const T &elem) { m_storage.push_back(elem); }
     void push(T &&elem) { m_storage.push_back(std::move(elem)); }
+    T &peek() { return m_storage.back(); }
     const T &peek() const { return m_storage.back(); }
     T pop() {
         auto ret = std::move(m_storage.back());
